a38189aad3eee5cfe48b09f73eb875f70fed4552
Nyasha Nallen Tumba <nyashatumba@gmail.com>
2025-05-02 08:47:44 +0200

    Intergrating the UI theme to the project
    Html and static files configurations

--- README.md
+++ README.md
@@ -13,3 +13,10 @@ Laravel Coffeshop Management System
         - Register a new user using the register link in the browser
         - Check the new created user details in the db users table.
 
+2. Intergrating the UI theme to the project
+    1. Copy all static files ( css, js, img, fonts, etc) & paste them under public folder in our project (create a new folder there called assets and paste them there)
+    2. Grab all links to link our static files from the index HTML file and add them in the <header> of the app.blade.php file of our project. App.blade file is in the folder resources/views/layouts
+    3. Configure these links to render our static files in blsde files like as follow.
+        <link href="{{ asset('assets/css/style.css') }}">
+    4. Do for both css and js files 
+    4. Edit the login, Register and home.blade with the designs from our theme's html files (the login, register & index.html files.)
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,93 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<section class="home-slider owl-carousel">
+
+    <div class="slider-item" style="background-image: url({{ asset ('assets/images/bg_2.jpg') }});">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row slider-text justify-content-center align-items-center">
+
+        <div class="col-md-7 col-sm-12 text-center ftco-animate">
+            <h1 class="mb-3 mt-5 bread">Register</h1>
+            <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Register</span></p>
+        </div>
+
+        </div>
+    </div>
+    </div>
+</section>
+
+<section class="ftco-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 ftco-animate">
+                <form method="POST" action="{{ route('register') }}" class="billing-form ftco-bg-dark p-3 p-md-5">
+                    @csrf
+                    <h3 class="mb-4 billing-heading">Register</h3>
+                    <div class="row align-items-end">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="Username">Username</label>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="Email">Email</label>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="Password">Password</label>
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="password-confirm">Confirm Password</label>
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="col-md-12">
+                            <div class="form-group mt-4">
+                                    <div class="radio">
+                                        <button type="submit" class="btn btn-primary py-3 px-4">Register</button>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <!-- END -->
+            </div>
+        </div>
+    </div>
+</section> 
+<!-- End section -->
+
+<!--div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -73,5 +159,5 @@
             </div>
         </div>
     </div>
-</div>
+</div-->
 @endsection
